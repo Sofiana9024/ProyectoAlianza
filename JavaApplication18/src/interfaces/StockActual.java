1d2bdcc8e8833d35bc2e37b2e37c2b521d27943a
Juan Fco <juanfcoesquivel0@gmail.com>
2024-05-16 23:28:30 -0600

    Stock de materiales

--- JavaApplication18/src/interfaces/StockActual.java
+++ JavaApplication18/src/interfaces/StockActual.java
@@ -21,67 +21,61 @@ public class StockActual extends javax.swing.JFrame {
         this.setTitle("Stock Actual");
         this.setResizable(false);
         
-        mostrarMateriales();
         mostrarCantidadTotal();
         mostrarCantidadDisponible();
         
     }
     
-    private void mostrarMateriales() {
-        try {
-            String query = "SELECT nom_mat FROM material";
-            PreparedStatement pst = con.prepareStatement(query);
-            ResultSet rs = pst.executeQuery();
-            
-            StringBuilder materiales = new StringBuilder();
-            while (rs.next()) {
-                materiales.append(rs.getString("nom_mat")).append("\n");
-            }
-            
-            profe1.setText(materiales.toString());
-        } catch (SQLException e) {
-            System.out.println("Error al obtener los materiales: " + e.getMessage());
-        }
-    }
-    
     private void mostrarCantidadTotal() {
         try {
-            String query = "SELECT SUM(stock_mat) AS total FROM material";
-            PreparedStatement pst = con.prepareStatement(query);
-            ResultSet rs = pst.executeQuery();
-
-            if (rs.next()) {
-                int total = rs.getInt("total");
-                salon1.setText(Integer.toString(total));
-            }
+        String query = "SELECT nom_mat, SUM(stock_mat) AS total FROM material GROUP BY nom_mat";
+        PreparedStatement pst = con.prepareStatement(query);
+        ResultSet rs = pst.executeQuery();
+        
+        StringBuilder materiales = new StringBuilder();
+        while (rs.next()) {
+            String nombreMaterial = rs.getString("nom_mat");
+            int cantidadTotal = rs.getInt("total");
+            materiales.append(nombreMaterial).append(": ").append(cantidadTotal).append("\n");
+        }
+        
+        salon1.setText(materiales.toString());
         } catch (SQLException e) {
-            System.out.println("Error al obtener la cantidad total: " + e.getMessage());
+            System.out.println("Error al obtener los materiales y su cantidad total: " + e.getMessage());
         }
     }
     
     private void mostrarCantidadDisponible() {
         try {
-            String query = "SELECT SUM(stock_mat) AS total_stock FROM material";
-            String queryPrestamo = "SELECT SUM(cant_pres) AS total_prestado FROM prestamo";
-
-            PreparedStatement pstStock = con.prepareStatement(query);
-            ResultSet rsStock = pstStock.executeQuery();
-            int totalStock = 0;
-            if (rsStock.next()) {
-                totalStock = rsStock.getInt("total_stock");
-            }
-
+        String queryMaterial = "SELECT nom_mat, SUM(stock_mat) AS total_stock FROM material GROUP BY nom_mat";
+        String queryPrestamo = "SELECT id_mat, SUM(cant_pres) AS total_prestado FROM prestamo GROUP BY id_mat";
+        
+        PreparedStatement pstMaterial = con.prepareStatement(queryMaterial);
+        ResultSet rsMaterial = pstMaterial.executeQuery();
+        
+        StringBuilder disponibilidadPorMaterial = new StringBuilder();
+        while (rsMaterial.next()) {
+            String nombreMaterial = rsMaterial.getString("nom_mat");
+            int totalStock = rsMaterial.getInt("total_stock");
+            
             PreparedStatement pstPrestamo = con.prepareStatement(queryPrestamo);
             ResultSet rsPrestamo = pstPrestamo.executeQuery();
+            
             int totalPrestado = 0;
-            if (rsPrestamo.next()) {
-                totalPrestado = rsPrestamo.getInt("total_prestado");
+            while (rsPrestamo.next()) {
+                if (rsPrestamo.getString("id_mat").equals(nombreMaterial)) {
+                    totalPrestado = rsPrestamo.getInt("total_prestado");
+                    break;
+                }
             }
-
+            
             int disponible = totalStock - totalPrestado;
-            salon.setText(Integer.toString(disponible));
+            disponibilidadPorMaterial.append(nombreMaterial).append(": ").append(disponible).append("\n");
+        }
+        
+        salon.setText(disponibilidadPorMaterial.toString());
         } catch (SQLException e) {
-            System.out.println("Error al obtener la cantidad disponible: " + e.getMessage());
+            System.out.println("Error al obtener la cantidad disponible por material: " + e.getMessage());
         }
     }
     /**
@@ -112,11 +106,8 @@ public class StockActual extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         salon = new javax.swing.JTextArea();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        profe1 = new javax.swing.JTextArea();
         jScrollPane3 = new javax.swing.JScrollPane();
         salon1 = new javax.swing.JTextArea();
-        jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel15 = new javax.swing.JLabel();
 
@@ -243,17 +234,7 @@ public class StockActual extends javax.swing.JFrame {
         salon.setRows(5);
         jScrollPane1.setViewportView(salon);
 
-        jPanel4.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 150, 340, 240));
-
-        profe1.setEditable(false);
-        profe1.setBackground(new java.awt.Color(255, 255, 255));
-        profe1.setColumns(20);
-        profe1.setFont(new java.awt.Font("Quicksand Book", 0, 18)); // NOI18N
-        profe1.setRows(5);
-        profe1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jScrollPane2.setViewportView(profe1);
-
-        jPanel4.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 150, 340, 240));
+        jPanel4.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 160, 340, 280));
 
         salon1.setEditable(false);
         salon1.setBackground(new java.awt.Color(255, 255, 255));
@@ -262,22 +243,17 @@ public class StockActual extends javax.swing.JFrame {
         salon1.setRows(5);
         jScrollPane3.setViewportView(salon1);
 
-        jPanel4.add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 150, 340, 240));
-
-        jLabel1.setFont(new java.awt.Font("Quicksand Bold", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Nombre");
-        jPanel4.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 110, -1, -1));
+        jPanel4.add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 160, 340, 280));
 
         jLabel3.setFont(new java.awt.Font("Quicksand Bold", 0, 24)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Cantidad total en existencia");
-        jPanel4.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 110, -1, -1));
+        jPanel4.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 120, -1, -1));
 
         jLabel15.setFont(new java.awt.Font("Quicksand Bold", 0, 24)); // NOI18N
         jLabel15.setForeground(new java.awt.Color(255, 255, 255));
         jLabel15.setText("Disponibles");
-        jPanel4.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(890, 110, -1, -1));
+        jPanel4.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 120, -1, -1));
 
         getContentPane().add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 210, 1200, 490));
 
@@ -329,7 +305,6 @@ public class StockActual extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
@@ -350,9 +325,7 @@ public class StockActual extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JTextArea profe1;
     private javax.swing.JTextArea salon;
     private javax.swing.JTextArea salon1;
     // End of variables declaration//GEN-END:variables
